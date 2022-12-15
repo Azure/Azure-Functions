29169ecb932572f528cb34a75259237d82f5237f
Mughundhan_R-MSFT <56299140+MughundhanRaveendran-MSFT@users.noreply.github.com>
2022-12-15 14:48:42 +0530

    (AzureCXP) fixes MicrosoftDocs/azure-docs#102758
    https://github.com/MicrosoftDocs/azure-docs/issues/102758

--- EOLHostMigration/CheckEOLAppsPerAzSub.ps1
+++ EOLHostMigration/CheckEOLAppsPerAzSub.ps1
@@ -1,10 +1,12 @@
 # This script lists outs Azure Function apps that are running on the v3 version of the host so they can be upgraded  
 # Review https://learn.microsoft.com/en-us/azure/azure-functions/migrate-version-3-version-4 for details on how to upgrade to ~4 of the runtime 
  
-$Subscription = '<YOUR SUBSCRIPTION ID>' 
- 
-Set-AzContext -Subscription $Subscription | Out-Null
+$subscriptions = Get-AzSubscription
 
+foreach ($sub in $subscriptions) {
+ 
+Set-AzContext -Subscription $sub| Out-Null
+ 
 $FunctionApps = Get-AzFunctionApp
 
 $AppInfo = @{}
@@ -18,6 +20,5 @@ foreach ($App in $FunctionApps)
 }
 
 $AppInfo
-$AppInfo.Count
 
-$AppInfo | Export-Csv -Path "$PSScriptRoot\YourEOLApps.csv" -NoTypeInformation
+}
